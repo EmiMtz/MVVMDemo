e7963117017b54ccbad6ba8a735b2b15747df92e
Emiliano Martinez <emiliano.martinez@wizeline.com>
2023-02-23 21:19:53 -0600

    MVVM Arq

---
+++ MVVMArquitectureDemo/Controller/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MVVMArquitectureDemo
+//
+//  Created by Emiliano Alfredo Martinez Vazquez on 23/02/23.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var viewModel = QuestionViewModel()
+    var quesitions:DataModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        viewModel.apiToGetQuestionData { [weak self] in
+            self?.quesitions = self?.viewModel.questionData
+
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        quesitions?.data?.questions?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = quesitions?.data?.questions?[indexPath.row].question
+        return cell
+    }
+
+
+}
---
+++ MVVMArquitectureDemo/ViewModel/QuestionViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  QuestionViewModel.swift
+//  MVVMArquitectureDemo
+//
+//  Created by Emiliano Alfredo Martinez Vazquez on 23/02/23.
+//
+
+import Foundation
+
+class QuestionViewModel {
+
+    var questionData:DataModel?
+    private let sourcesURL = URL(string: "https://quiz-68112-default-rtdb.firebaseio.com/quiz.json")!
+
+    func apiToGetQuestionData(completion : @escaping () -> ()) {
+
+        URLSession.shared.dataTask(with: sourcesURL) { [weak self] (data, urlResponse, error) in
+            if let data = data {
+
+                let jsonDecoder = JSONDecoder()
+
+                let empData = try! jsonDecoder.decode(DataModel.self, from: data)
+                self?.questionData = empData
+                completion()
+            }
+        }.resume()
+    }
+}
